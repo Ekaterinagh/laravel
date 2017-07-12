4743cb61bc6aa7062428800808b2297446843789
ekaterinagh <sobaka@gmail.com>
2017-07-12 15:44:52 +0300

    commit_120717

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,51 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-    </head>
-    <body>
-		<h1>Hello World!</h1>
-		<div>{{$id}}</div>
-    </body>
-</html>
+@extends('layouts.main')
+@section('content')
+<h3>TATOUAGE & PIERCING </h3>
+<p>American Body Art, salons de Tatouage et Piercing sont présents à Paris, principalement à Chatelet depuis bientôt vingt ans.
+
+A Paris, les salons de tatouage et piercing American Body Art sont une institution. Le tatouage à Paris est un art de vivre.
+
+Mille significations aux tatouages. Tatouage homme, tatouage femme, tatouage avant bras, tatouage poignet, c'est plusieurs milliers de modèles que nous avons réalisés depuis notre ouverture à Paris dans la fin des années 90.
+
+La tatouage est un mode de vie, il n'a rien de temporaire. C'est pourquoi, chez American Body Art, nous avons quatre règles, toutes les quatre étant aussi importantes les unes que les autres. La première règle de nos tatouages: l'hygiène. La seconde: le talent. La troisième: l'hygiène. Et la quatrième règle est le talent.
+
+Et enfin, si nous pouvions en rajouter deux autres, celles ci seraient l'hygiène et le talent... Pour le piercing, nos règles sont les mêmes. Nos artistes perceurs ornent nos clients avec passion, propreté et assurent un suivi personnalisé de leurs interventions.
+
+La philosophie de American Body Art demeure le client avant tout, et l'esprit Rock & Roll. L'accueil et le suivi est primordial pour nous, car nous faisons notre metier depuis plusieurs décénies, avec toujours le même souci du travail, non pas bien fait, mais parfaitement éxécuté.
+
+A Paris, en Floride, en Australie, au Liban, si vous voulez vous faire tatouer ou percer...
+
+Vous pouvez vous rendre chez AMERICAN BODY ART.</p>
+@stop
+@section('aba_innocent')
+<h3><span>American body Art</span> Paris - Innocent</h3>
+<p>7 rue des Innocents Paris 1er tel: 01 40 26 51 94 <a href="#">innocent@americanbodyart.fr</a></p>
+<p>ACCES Paris Metro Chatelet: Ligne 1 - 4 - 7 - 14 Rer Chatelet les halles: Ligne A - B - D</p>
+@stop
+@section('aba_sebastopol')
+<h3><span>American body Art</span> Paris - Sebastopol</h3>
+<p>24 blvd de Sebatopol Paris 4e tel: 01 40 29 15 80
+<a href="#">sebastopol@americanbodyart.fr</a></p>
+<p>ACCES Paris Metro Chatelet: Ligne 1-4-7-14 Rer Chatelet les Halles: Ligne A-B-D</p>
+@stop
+@section('aba_opera')
+<h3><span>American body Art</span> Paris - Opéra</h3>
+<p>4 Rue Joubert Paris 09 tel: 01 53 16 15 96 <a href="#">opera@americanbodyart.fr</a></p>
+<p>ACCES Paris Metro Opéra: Ligne 3 - 9 - 9  Rer Opéra: Ligne A</p>
+</br>
+@stop
+@section('aba_magic')
+<h3><span>American body Art</span> Paris - Magic Circus</h3>
+<p>44 rue des lombars Paris 01 tel: 01 40 26 48 38 <a href="#">magiccircus@americanbodyart.fr</a></p>
+<p>ACCES Paris Metro Chatelet: Ligne 1 - 4 - 7 - 14 Rer Chatelet les halles: Ligne A - B - D</p>
+@stop
+@section('aba_lombard')
+<h3><span>American body Art</span> Paris - Lombard</h3>
+<p>25 rue des lombards Paris 4e tel: 01 40 29 15 <a href="#">lombard@americanbodyart.fr</a></p>
+<p>ACCES Paris Metro Chatelet: Ligne 1 - 4 - 7 - 14 Rer Chatelet les halles: Ligne A - B - D</p>
+@stop
+@section('aba_rambuteau')
+<h3><span>American body Art</span> Paris - Rambuteau</h3>
+<p>79 rue rambuteau Paris 1er tel: 01 40 26 54 83 <a href="#">@americanbodyart.fr</a></p>
+<p>ACCES Paris Metro Chatelet: Ligne 1 - 4 - 7 - 14 Rer Chatelet les halles: Ligne A - B - D</p>
+@stop
